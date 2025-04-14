81d66c6ef76f52e38f584016a40faf5f4f26ec08
TysikGG <avdej.vitalij@gmail.com>
2025-04-14 21:51:42 +0300

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "@/styles/global.css";
 
 export const metadata: Metadata = {
   title: "Reflow"
@@ -11,6 +12,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet"/>
+      </head>
       <body>
         {children}
       </body>
