5727922aca30e7df8243a32289dad0fa8499137d
TysikGG <avdej.vitalij@gmail.com>
2025-04-14 22:05:30 +0300

    chats init

---
+++ src/UI/chats.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/components/chats.module.css";
+
+export default function Chats() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+}
