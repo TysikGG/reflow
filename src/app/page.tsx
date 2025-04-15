d226504654ac230dd2526f4e7d21de7718b45f1b
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-04-15 16:00:30 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Chats from "@/UI/chats";
 import Panel from "@/UI/panel";
 
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div className="home">
       <Panel />
+      <Chats />
     </div>
   );
 }
