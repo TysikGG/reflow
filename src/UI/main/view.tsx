6f0832174f14cef0525f88597504b46fa08da512
TysikGG <avdej.vitalij@gmail.com>
2025-04-16 21:55:21 +0300

    view section

---
+++ src/UI/main/view.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/components/main/view.module.css";
+
+export default function View() {
+    return (
+        <section className={styles.container}>
+            
+        </section>
+    )
+}
