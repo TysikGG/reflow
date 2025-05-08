560f974424f9026c91f9a0bb10158a036a290427
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-08 23:28:11 +0300

    form login & registration

--- src/UI/auth/form.tsx
+++ src/UI/auth/form.tsx
@@ -1,29 +1,97 @@
-"use client"
+'use client';
 import styles from "@/styles/app/auth/auth.module.css";
 
 import Key from "@/favicons/svg/Key.svg";
+import { MouseEventHandler, ReactNode, useState } from "react";
+
+interface BaseAuthFormProps {
+    title: string,
+    tip: string,
+    inputs: ReactNode,
+    buttonTitle: string,
+    secondary?: boolean,
+    secondaryTitle?: string,
+    secondaryButtonTitle?: ReactNode,
+    tipCallback: MouseEventHandler,
+    secondaryCallback?: MouseEventHandler
+}
 
 export default function AuthForm() {
-    return (
-        <div className={styles.container}>
-            <section className={styles.loginContainer}>
-                <form className={styles.loginForm}>
-                    <div className={styles.loginTitleContainer}>
-                        <h1>Авторизация</h1>
-                        <h3>У меня нету аккаунта</h3>
-                    </div>
-                    <div className={styles.inputsContainer}>
-                        <input name="username" placeholder="Логин" />
-                        <input name="password" type="password" placeholder="Пароль" />
-                    </div>
-                    <h3>ИЛИ</h3>
-                    <button className={styles.buttonSecondary}>
-                        <Key />
-                        Войти по ключу
-                    </button>
-                    <button>Войти</button>
-                </form>
-            </section>
-        </div>
-    )
+    const [formType, setFormType] = useState<String>("login");
+
+    function BaseAuthForm({ title, tip, inputs, buttonTitle, secondary, secondaryTitle, secondaryButtonTitle, tipCallback, secondaryCallback }: BaseAuthFormProps) {
+        return (
+            <div className={styles.container}>
+                <section className={styles.loginContainer}>
+                    <form className={styles.loginForm}>
+                        <div className={styles.loginTitleContainer}>
+                            <h1>{title}</h1>
+                            <h3 onClick={tipCallback}>{tip}</h3>
+                        </div>
+                        {inputs}
+                        <h3 style={{ display: secondary ? "flex" : "none" }}>
+                            {secondaryTitle}
+                        </h3>
+                        <button
+                            style={{ display: secondary ? "flex" : "none" }}
+                            className={styles.buttonSecondary}
+                            onClick={secondaryCallback}
+                        >
+                            {secondaryButtonTitle}
+                        </button>
+                        <button>{buttonTitle}</button>
+                    </form>
+                </section>
+            </div>
+        )
+    }
+
+    function AuthFormInputsConstructor(children: any) {
+        return (
+            <div className={styles.inputsContainer}>
+                {...children.children}
+            </div>
+        )
+    }
+
+    if (formType === "login") {
+        return (
+            <BaseAuthForm
+                title="Авторизация"
+                tip="У меня нету аккаунта"
+                inputs={
+                    <AuthFormInputsConstructor
+                        children={[
+                            <input name="username" placeholder="Логин" />,
+                            <input name="password" type="password" placeholder="Пароль" />
+                        ]}
+                    />
+                }
+                buttonTitle="Войти"
+                tipCallback={() => setFormType("register")}
+                secondary={true}
+                secondaryTitle="ИЛИ"
+                secondaryButtonTitle={(<><Key /> Войти по ключу</>)}
+            />
+        )
+    } else if (formType === "register") {
+        return (
+            <BaseAuthForm
+                title="Регистрация"
+                tip="У меня уже есть аккаунт"
+                inputs={
+                    <AuthFormInputsConstructor
+                        children={[
+                            <input name="username" placeholder="Придумайте логин" />,
+                            <input name="email" type="email" placeholder="Ваш email" />,
+                            <input name="password" type="password" placeholder="Придумайте пароль" />
+                        ]}
+                    />
+                }
+                buttonTitle="Регистрация"
+                tipCallback={() => setFormType("login")}
+            />
+        )
+    }
+
 }
