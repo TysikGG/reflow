f00d27c927a105c510c04bb73f6e059ffaf9bda3
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-08 23:28:35 +0300

    Update Auth.ts

--- src/libs/API/users/Auth.ts
+++ src/libs/API/users/Auth.ts
@@ -1,5 +1,27 @@
-class APIUserAuth {
-    register() {
-        
+import request from "../Request";
+
+interface APIUserAuthProps {
+    username: string,
+    email: string,
+    hashed_password: string
+}
+
+export default class APIUserAuth implements APIUserAuthProps {
+    username: string
+    email: string
+    hashed_password: string
+
+    constructor({ username, email, hashed_password }: APIUserAuthProps) {
+        this.username = username;
+        this.email = email;
+        this.hashed_password = hashed_password
+    }
+
+    async register() {
+        await request("/users/register", {data: {
+            username: this.username,
+            email: this.email,
+            hashed_password: this.hashed_password
+        }})
     }
 }
