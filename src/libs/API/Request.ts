5a9f04ced6f03cd707f4cfb05d6fcb771a3554a8
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-08 23:28:48 +0300

    request function (dev)

--- src/libs/API/Request.ts
+++ src/libs/API/Request.ts
@@ -1,29 +1,8 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const uri = "http://127.0.0.0:8080/api/v1/"
 
-interface RequestData {
-    method: string,
-    url: string,
-    data: object
-}
-
-export class Request implements RequestData {
-    method: string;
-    url: string;
-    data: object;
-
-    constructor({ method, url, data }: RequestData) {
-        this.method = method;
-        this.url = url;
-        this.data = data;
-    }
-
-    async send() {
-        return await axios({
-            method: this.method,
-            url: this.url,
-            data: this.data
-        });
-    }
+export default async function request(route: string, { method, data }: AxiosRequestConfig) {
+    const url = uri + route;
+    return await axios({ method, url, data });
 }
