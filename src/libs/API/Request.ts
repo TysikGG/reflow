48dda905720dd82343293d3f13cd00fdac344de8
TysikGG <avdej.vitalij@gmail.com>
2025-05-09 21:10:31 +0300

    Update Request.ts

--- src/libs/API/Request.ts
+++ src/libs/API/Request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const uri = "http://127.0.0.0:8080/api/v1/"
+const uri = "http://127.0.0.0:8080/api/v1"
 
 export default async function request(route: string, { method, data }: AxiosRequestConfig) {
     const url = uri + route;
