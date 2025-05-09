a4052e04b02902f9b0b7d6533b286eff5182a8a9
TysikGG <avdej.vitalij@gmail.com>
2025-05-09 21:11:11 +0300

    Create tip.ts

---
+++ src/libs/components/tip.ts
@@ -0,0 +1,29 @@
+import { Slide, toast } from "react-toastify";
+
+export function showSuccessMessage(message: string) {
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Slide,
+    });
+}
+
+export function showErrorMessage(message: string) {
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Slide,
+    });
+}
