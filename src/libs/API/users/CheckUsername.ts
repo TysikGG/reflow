809862b0104494d3a1602718f8d17663eee2a296
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-16 15:23:44 +0300

    username check function

---
+++ src/libs/API/users/CheckUsername.ts
@@ -0,0 +1,10 @@
+import request from "../Request";
+
+export default async function checkUsername(username: any) {
+    const data = await request("/users/auth/check", {
+        method: "POST",
+        data: { username }
+    });
+
+    return data;
+}
