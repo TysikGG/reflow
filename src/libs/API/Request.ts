69a3d9cabaf1f8fca60e40e721f0fb89981c20d8
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-19 00:59:17 +0300

    requests errors handler

--- src/libs/API/Request.ts
+++ src/libs/API/Request.ts
@@ -1,8 +1,17 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { showErrorMessage } from "../components/tip";
 
 const uri = "http://localhost:8080/api/v1"
 
-export default async function request(route: string, { method = "post", data }: AxiosRequestConfig) {
+export default async function request(
+    route: string,
+    { method = "post", data }: AxiosRequestConfig
+): Promise<AxiosResponse> {
     const url = uri + route;
-    return await axios({ method, url, data });
+    try {
+        return await axios({ method, url, data });
+    } catch (error) {
+        showErrorMessage(`${error}`);
+        throw error;
+    }
 }
