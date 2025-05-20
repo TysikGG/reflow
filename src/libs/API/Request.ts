ed4920eaf44ec2e170d1bb48922992e77f57c686
TysikGG <100859234+TysikGG@users.noreply.github.com>
2025-05-20 15:33:06 +0300

    Update Request.ts

--- src/libs/API/Request.ts
+++ src/libs/API/Request.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { Axios, AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { showErrorMessage } from "../components/tip";
 
 const uri = "http://localhost:8080/api/v1"
@@ -11,7 +11,20 @@ export default async function request(
     try {
         return await axios({ method, url, data });
     } catch (error) {
-        showErrorMessage(`${error}`);
+        let errorMessage = error;
+
+        if (error instanceof AxiosError) {
+            const axiosError = error as AxiosError;
+            const responseData = axiosError["response"];
+            if (responseData) {
+                const response = responseData.data as { localeStatus: string };
+                if (response.localeStatus) {
+                    errorMessage = response.localeStatus;
+                }
+            }
+        };
+
+        showErrorMessage(String(errorMessage));
         throw error;
     }
 }
